d1e80d48868385af9d4ae1a51f9c8630ac2618ce
Hojiakbar <hhgta4146@gmail.com>
2026-02-02 02:09:13 +0500

    chore(debug): add debug overlay for call state

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -6,6 +6,7 @@ import { AuthProvider } from "../lib/auth";
 import { LanguageProvider } from "../lib/i18n";
 import { CallProvider } from "../context/CallContext";
 import CallModal from "../components/CallModal";
+import DebugCall from "../components/DebugCall";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
@@ -16,6 +17,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         <AuthProvider>
           <CallProvider>
             <CallModal />
+            <DebugCall />
             {children}
           </CallProvider>
         </AuthProvider>
